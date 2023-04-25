1a798740250c338482dcb8270e3a695cb3218b3f
Farzad-sajjadi <farzadsajadi1381@gmail.com>
2023-04-25 11:44:07 +0430

    .

---
+++ pages/03-ios/js/ios.js
@@ -0,0 +1,9 @@
+function gephFun() {
+    document.getElementById('geph-main').style.display = "none"
+    document.getElementById('geph-det').style.display = "block"
+}
+
+function gephFun2() {
+    document.getElementById('geph-main').style.display = "block"
+    document.getElementById('geph-det').style.display = "none"
+}
